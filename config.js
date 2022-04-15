c9e190ca01c8f52853c8de519265ca06e7310fe0
FrovaHappy <happypro5.0@gmail.com>
2022-04-15 17:22:17 -0300

    primer commit

---
+++ config.js
@@ -0,0 +1,5 @@
+// var env
+import 'dotenv/config';
+export const token = process.env.DISCORD_TOKEN;
+export const clientId = process.env.DISCORD_CLIENT_ID;
+export const guildId = process.env.DISCORD_GUILD_ID;
---
+++ index.js
@@ -0,0 +1,28 @@
+import {token} from './config.js';
+import { Client, Intents } from 'discord.js';
+import deployCommands from './deploy-commands.js';
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+deployCommands();
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
