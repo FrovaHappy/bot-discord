2397328851b20ce81f548c693373581fde6cad61
FrovaHappy <happypro5.0@gmail.com>
2022-05-06 20:02:32 -0300

    commands in global or for guilds

--- config.js
+++ config.js
@@ -6,10 +6,12 @@ const clientId = process.env.DISCORD_CLIENT_ID;
 const guildId = ['951875023868686336' , '925085954685956156'];
 export let timeIds = new Map();
 const getDbHost = process.env.MONGODB_HOST || "mongodb://localhost:27017/bot-test";
+const guildGlobal = process.env.GUILD_GLOBAL || false;
 
 const configurations = {
     DISCORD_CLIENT_ID: clientId,
     DISCORD_GUILD_ID: guildId,
+    DISCORD_GUILD_GLOBAL: guildGlobal,
     DISCORD_TOKEN: token,
     MONGODB_HOST: getDbHost
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -17,17 +17,31 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(config.DISCORD_TOKEN);
 
 (async () => {
-	for (const guild of config.DISCORD_GUILD_ID) {
-		console.log('Registering Guild :',guild);
+	if (config.DISCORD_GUILD_GLOBAL){
 		try {
-			console.log('···Started refreshing application (/) commands.');
+			console.log('refreshing (/) commands in Global.');
+
+			await rest.put(
+				Routes.applicationCommands(config.DISCORD_CLIENT_ID),
+				{ body: commands },
+			);
+
+			console.log('(/) commands ··· Ok.');
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	
+	for (const guild of config.DISCORD_GUILD_ID) {
+		try {
+			console.log(`refreshing (/) commands in ${guild}.`);
 
 			await rest.put(
 				Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, guild),
 				{ body: commands },
 			);
 
-			console.log('···Successfully reloaded application (/) commands.');
+			console.log('(/) commands ··· Ok.');
 		} catch (error) {
 			console.error(error);
 		}
