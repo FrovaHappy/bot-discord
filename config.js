626bdde5a673998f90f3cde9b9fe00bbe7e0e97b
FrovaHappy <happypro5.0@gmail.com>
2022-05-15 10:41:29 -0300

    agregado database

--- config.js
+++ config.js
@@ -5,8 +5,10 @@ const token = process.env.DISCORD_TOKEN;
 const clientId = process.env.DISCORD_CLIENT_ID;
 const guildId = ['951875023868686336' , '925085954685956156'];
 export let timeIds = new Map();
-const getDbHost = process.env.MONGODB_HOST || "mongodb://localhost:27017/bot-test";
 const guildGlobal = process.env.GUILD_GLOBAL || false;
+
+let getDbHost = process.env.MONGODB_HOST || "mongodb://localhost:27017/<database>";
+getDbHost = getDbHost.replace('<database>', process.env.DATABASE_NAME);
 
 const configurations = {
     DISCORD_CLIENT_ID: clientId,
---
+++ database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import config from "./config";
+
+(async () => {
+  try {
+    const db = await mongoose.connect(config.MONGODB_HOST);
+    console.log("Mongodb is connected to", db.connection.host);
+  } catch (error) {
+    console.error(error);
+  }
+})();
