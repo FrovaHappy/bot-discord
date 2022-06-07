29385118bc41166a5b60b167e32abcaefa00df01
FrovaHappy <happypro5.0@gmail.com>
2022-06-06 23:17:40 -0300

    filtering db keys for the buildersDbChange.

--- src/commands/countdown/utils/builderDbchange.js
+++ src/commands/countdown/utils/builderDbchange.js
@@ -1,11 +1,23 @@
 export function buildersDbChange(countdownQuery) {
   const hasUpdated = countdownQuery.hasUpdated;
   const data = countdownQuery.data;
-  const change = Object.keys(hasUpdated);
+  const change = Object.keys(keysIgnore(hasUpdated));
   let result = ``;
+  if (change.length) {
+    result = "> creo que estoy vacio.";
+    return result;
+  }
   for (const key of change) {
-    if (hasUpdated[key]) result += `> :white_medium_small_square: ${key}: \`${data[key]}\` \n`;
+    if (hasUpdated[key])
+      result += `> :white_medium_small_square: ${key}: \`${data[key]}\` \n`;
     else result += `> :black_medium_small_square: ${key}: \`${data[key]}\` \n`;
   }
   return result;
+}
+function keysIgnore(hasUpdated) {
+  const ignore = ["_id", "guild", "__v", "createdAt", "updatedAt"];
+  for (const key of ignore) {
+    delete hasUpdated[key];
+  }
+  return hasUpdated;
 }
