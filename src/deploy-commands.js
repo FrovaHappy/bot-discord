b827c7425d9ea955802420b3febc85d6ac7ace8e
FrovaHappy <happypro5.0@gmail.com>
2022-06-09 20:42:23 -0300

    fix duplication of commands.

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -19,32 +19,54 @@ const rest = new REST({ version: '9' }).setToken(config.DISCORD_TOKEN);
 (async () => {
 	if (config.DISCORD_GUILD_GLOBAL){
 		try {
+			config.DISCORD_GUILD_ID.map(guildId => deleteGuildCommands(guildId))
 			console.log('refreshing (/) commands in Global.');
-
 			await rest.put(
 				Routes.applicationCommands(config.DISCORD_CLIENT_ID),
 				{ body: commands },
 			);
-
 			console.log('(/) commands ··· Ok.');
 		} catch (error) {
 			console.error(error);
 		}
 		return
 	}
-	
-	for (const guild of config.DISCORD_GUILD_ID) {
+	await deleteCommands()
+	config.DISCORD_GUILD_ID.map(guild => {
 		try {
-			console.log(`refreshing (/) commands in ${guild}.`);
-
-			await rest.put(
+			console.log(`(/) refreshing  commands in ${guild}.`);
+			rest.put(
 				Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, guild),
 				{ body: commands },
 			);
-
-			console.log('(/) commands ··· Ok.');
+			console.log('(/) refreshing ··· OK');
 		} catch (error) {
 			console.error(error);
 		}
-	}
+	})
 })();
+async function deleteCommands() {
+  const query = await rest.get(
+    Routes.applicationCommands(config.DISCORD_CLIENT_ID)
+  );
+  console.log("(/) deleting all commands");
+  query.map((command) =>
+    rest.delete(Routes.applicationCommand(config.DISCORD_CLIENT_ID, command.id))
+  );
+}
+async function deleteGuildCommands(guildId) {
+  const query = await rest.get(
+    Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, guildId)
+  );
+  console.log("(/) deleting all guild commands");
+  query.map((command) =>
+    rest.delete(
+      Routes.applicationGuildCommand(
+        config.DISCORD_CLIENT_ID,
+        guildId,
+        command.id
+      )
+    )
+  );
+  console.log("(/) deleting ... OK");
+}
