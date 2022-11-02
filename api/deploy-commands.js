b4b08f443ebb258cc0ea8c05a9dd1e5733fe7632
FrovaHappy <happypro5.0@gmail.com>
2022-11-02 16:15:59 -0300

    implementation of new command filter

--- api/deploy-commands.js
+++ api/deploy-commands.js
@@ -1,34 +1,36 @@
-import config from '../config.js';
-import searchCommands from './util/searchCommands.js';
-import filterCommands from './util/filterCommands.js';
+import config from '../config.js'
+import searchCommands from './util/searchCommands.js'
+import filterCommands from './util/filterCommands.js'
 import {
   putApplicationCommands,
   putApplicationGuildsCommands,
   deleteApplicationCommands,
-  deleteApplicationGuildsCommands,
-} from "./util/execute.js"
+  deleteCommand,
+} from './util/execute.js'
 
 const applicationId = config.DISCORD_CLIENT_ID
 const guildsIds = config.DISCORD_GUILD_ID
 
-const filterPremium = ['countdown']
-const filterPublic = []
-const filterDev = ['log'];
+const servers = {
+  premium: [],
+  global: ['countdown'],
+}
 
-export async function deployCommands(){
+export async function deployCommands() {
   let result = {}
   const commands = await searchCommands()
-  const commandsPremium = filterCommands(filterPremium, commands);
-  const commandsGlobal =  filterCommands(filterPublic, commands);
-  const commandsDeveloper =  filterCommands(filterDev, commands);
+  const commandsFiltert = filterCommands(servers, commands)
+  const commandsPremium = commandsFiltert.premium
+  const commandsGlobal = commandsFiltert.global
+  const commandsDeveloper = commandsFiltert.developer
 
+  //deleteCommand(applicationId, '925085954685956156')
   //public commands
-  result.servPublic = await putApplicationCommands( applicationId, commandsGlobal )
-  if ( !result.servPublic.done ) {
-    result.servPublicdeleted = await deleteApplicationCommands( applicationId )
+  result.servPublic = await putApplicationCommands(applicationId, commandsGlobal)
+  if (!result.servPublic.done) {
+    result.servPublicdeleted = await deleteApplicationCommands(applicationId)
   }
-  result.servDev = await putApplicationGuildsCommands( applicationId, guildsIds, commandsDeveloper)
-  //result.deleteservDev= await deleteApplicationGuildsCommands( applicationId, guildsIds)
-  
+  result.servDev = await putApplicationGuildsCommands(applicationId, guildsIds, commandsDeveloper)
+
   return result
 }
--- api/util/execute.js
+++ api/util/execute.js
@@ -1,84 +1,111 @@
-("use estrict");
+;('use estrict')
 //@ts-check
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
-import config from "../../config.js"
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v10'
+import config from '../../config.js'
+import { SlashCommandBuilder } from '@discordjs/builders'
+const deletingCommands = new SlashCommandBuilder().setName('delete-commands').setDescription('Delete')
 
-const rest = new REST({ version: "9" }).setToken(config.DISCORD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(config.DISCORD_TOKEN)
 
+export async function deleteCommand(applicationId, guildsId) {
+  await rest
+    .put(Routes.applicationGuildCommands(applicationId, guildsId), { body: [deletingCommands.toJSON()] })
+    .catch((e) => {
+      console.log('en el put')
+    })
+  const commandfodeleting = await rest.get(Routes.applicationGuildCommands(applicationId, guildsId)).catch((e) => {
+    console.log('en el get')
+  })
+  await Promise.all(
+    commandfodeleting.map((command) => {
+      rest.delete(Routes.applicationGuildCommand(applicationId, guildsId, command.id)).catch((e) => {
+        console.log('en el delete')
+      })
+    })
+  )
+}
 /**
- * publica en los sevidores privados ej. (serv premium y dev)
- * @param {string[]} guildsIds 
- * @param {Array} commands 
+ * Publica en los sevidores privados ej. (serv premium y dev)
+ * @param {string[]} guildsIds
+ * @param {Array} commands
  * @returns un array de objetos que contiene el guild.id y el estado que a finalizado
  */
-export async function putApplicationGuildsCommands( applicationId, guildsIds, commands) {
+export async function putApplicationGuildsCommands(applicationId, guildsIds, commands) {
   let result = []
   guildsIds.map((guildId) => {
     const fullroute = Routes.applicationGuildCommands(applicationId, guildId)
     result.push(
-      rest.put(fullroute, { body: commands } )
-        .then( 
-          (_resolve) => { return { guildId, done: true, commands };},
-          (_reject) => { return { guildId, done: false, commands };}
-        )
+      rest.put(fullroute, { body: commands }).then(
+        (_resolve) => {
+          return { guildId, done: true, commands }
+        },
+        (_reject) => {
+          return { guildId, done: false, commands }
+        }
+      )
     )
   })
   return await Promise.all(result)
 }
 /**
  * @param {string} applicationId
- * @param {array} commands 
+ * @param {array} commands
  * @returns {object} si se resolvio sastifacotiamente
  */
-export async function putApplicationCommands( applicationId, commands ) {
+export async function putApplicationCommands(applicationId, commands) {
   let commandsName = []
-  commands.map( command => commandsName.push( command.name ))
+  commands.map((command) => commandsName.push(command.name))
   if (commands.length === 0) return false
   const fullroute = Routes.applicationCommands(applicationId)
-  const result = await rest.put( fullroute, { body: commands,} )
-    .then (
-      (_resolve) => { return {done: true, commandsName} },
-      (_reject) => { return {done: false, commandsName} }
-    )
-  return result 
+  const result = await rest.put(fullroute, { body: commands }).then(
+    (_resolve) => {
+      return { done: true, commandsName }
+    },
+    (_reject) => {
+      return { done: false, commandsName }
+    }
+  )
+  return result
 }
 /**
  * @param {string} applicationId
- * @param {string[]} guildsIds 
+ * @param {string[]} guildsIds
  */
-export async function deleteApplicationGuildsCommands( applicationId, guildsIds ){
+export async function deleteApplicationGuildsCommands(applicationId, guildsIds) {
   let result = []
-  result = guildsIds.map(guildId =>{
+  result = guildsIds.map((guildId) => {
     const fullroute = Routes.applicationGuildCommands(applicationId, guildId)
-    return  deleteForCommandId( fullroute )
+    return deleteForCommandId(fullroute)
   })
-  result = await Promise.all( result )
-  result.map((obj, index) => { 
+  result = await Promise.all(result)
+  result.map((obj, index) => {
     obj.guildId = guildsIds[index]
     return obj
   })
   return result
 }
-export async function deleteApplicationCommands( applicationId ){
-    const fullroute = Routes.applicationCommands(applicationId)
-    return await deleteForCommandId( fullroute )
+export async function deleteApplicationCommands(applicationId) {
+  const fullroute = Routes.applicationCommands(applicationId)
+  return await deleteForCommandId(fullroute)
 }
 // ? refactorizar haciendo un put en un comando vacio y luego eliminarlo(para evitar tantos deletes)
-export async function deleteForCommandId( fullroute ) {
+export async function deleteForCommandId(fullroute) {
   let result = []
   let count = 0
-  const query = await rest.get(fullroute);
-  query.map( command => {
-    const fullrouteCommandId = fullroute+ "/" + command.id
+  const query = await rest.get(fullroute)
+  query.map((command) => {
+    const fullrouteCommandId = fullroute + '/' + command.id
     result.push(
-      rest.delete(fullrouteCommandId)
-        .then (
-          (_resolve) => { 
-            count++
-            return { name: command.name, ok: 1 }},
-          (_reject) => { return { name: command.name, ok: 0 }}
-        )
+      rest.delete(fullrouteCommandId).then(
+        (_resolve) => {
+          count++
+          return { name: command.name, ok: 1 }
+        },
+        (_reject) => {
+          return { name: command.name, ok: 0 }
+        }
+      )
     )
   })
   result = await Promise.all(result)
