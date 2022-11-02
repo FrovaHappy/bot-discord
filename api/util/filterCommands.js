c7f497d59a963ec69f0f54184edd62058d9dbac7
FrovaHappy <happypro5.0@gmail.com>
2022-11-02 16:15:12 -0300

    filter of commands for group

--- api/util/filterCommands.js
+++ api/util/filterCommands.js
@@ -1,14 +1,31 @@
 //@ts-check
-
 /**
-* @param {string[]} commandsNameFilter 
-* @param {array} commands
-*/
-function filterCommands(commandsNameFilter, commands) {
-  let result = commandsNameFilter.map((name) => {
-    return commands.filter((command) => command.name === name)[0];
-  });
-  return result.filter((isDefined) => isDefined);
+ * @param {{premium:string[],global:string[]}} commandsToFilter
+ * @param {import('@discordjs/builders').SlashCommandBuilder[]} slashCommands
+ * @return returns an object with slashCommands servers.
+ */
+function filterCommands(commandsToFilter, slashCommands) {
+  const serversNames = Object.keys(commandsToFilter)
+  let developer = slashCommands
+  let servers = {}
+  serversNames.map((serverName) => {
+    servers[serverName] = commandsToFilter[serverName]
+      .map((commandName) => {
+        //search the slash commands declared in the commandtofilter
+        const slashCommandsFound = slashCommands.filter((commnand) => {
+          return commnand.name == commandName
+        })
+        //the slash commands found are removed from developer
+        slashCommandsFound.map((slashCommandFound) => {
+          developer = developer.filter((slashCommandInDev) => {
+            return !(slashCommandInDev == slashCommandFound)
+          })
+        })
+        return slashCommandsFound
+      })
+      .flat()
+  })
+  servers.developer = developer
+  return servers 
 }
-
-export default filterCommands;
+export default filterCommands
